37c2fbf899ee1b8fc2be2b62a92f6e14722a09a3
Calvin Ting <calvinting@protonmail.com>
2018-02-28 10:48:33 -0800

    added footer,header and page submit

--- footer.php
+++ footer.php
@@ -11,7 +11,29 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+
+
+
+
+				<?php 
+				wp_nav_menu(
+				array(
+				'theme_location' =>'primary',
+				'menu_id' => 'primary-menu',
+				'menu_class' => 'footer-navigation'	
+				)	
+				);
+
+				?>
+
+
+					<p>Brought to you by &copy; <a href="https://github.com/calvintyvm">Calvin Ting </a>
+					<?php echo date('Y') ;?></p>
+
+
+
+
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
