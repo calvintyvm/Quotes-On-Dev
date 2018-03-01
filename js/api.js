870c9a8c969185d912f3a8221738abb6b71ea518
Calvin Ting <calvinting@protonmail.com>
2018-03-01 09:18:10 -0800

    finished jquery button on click with get method on front page

--- js/api.js
+++ js/api.js
@@ -1,6 +1,28 @@
 (function($){
 
+//fetch a random quote post http://localhost:8888/project5/wp-json/wp/v2/posts
 
+//load all the posts and count them and then do a random number from 1 to whatver
 
+//history api mdn HISTORYpushstate
+
+//submit a new quote with the form using jquery 
+
+$( '#new-quote-button' ).on( 'click', function ( s ) {
+    s.preventDefault();
+    $.ajax({
+      url: '/project5/wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+          success: function ( data ) {
+
+            $(".entry-title").empty();
+            $(".entry-title").append(data[0].title.rendered);
+            $(".entry-content").empty();
+            $(".entry-content").append(data[0].content.rendered);
+            $(".source").empty();
+            $(".source").append('<a href="' + data[0]._qod_quote_source_url + '">' + data[0]._qod_quote_source + '</a>');
+
+    }});
+
+  } );
 
 })(jQuery);
